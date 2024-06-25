266a9fe681e71163c087489737631744842734ef
estelaoli23 <estelaoliveirareato23@outlook.com>
2024-06-25 19:42:19 -0300

    feat:first-lesson-sql

---
+++ aula-sql.sql
@@ -0,0 +1,19 @@
+-- Os principais comandos SQL
+
+-- DQL - Linguagem consulta de dados
+
+ SELECT * FROM tabela; -- Seleciona todos os campos de uma tabela
+
+ -- DML --> Linguagem de manipulação de dados da tabela
+
+ INSERT, UPDATE, DELETE
+
+ -- Insere, atualiza e deleta dados do banco de dados
+
+ -- DDL --> Linguagem de Definição de dados - criação de tabelas
+
+ CREATE
+ ALTER
+ DROP
+
+ -- cria, altera e apaga tabelas
